ba584215fa5131461b423f2cbb42637418866e74
Patrick Pragman <patrickpragman@pop-os.localdomain>
2022-10-04 22:40:20 -0800

    added some terrible error handling

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
 
 function populate() {
-    console.log("loading data");
-    let station_list = document.getElementById("station_list");
-    let search_bar = document.getElementById("search_bar")
-    let bus_stations = '';
-    let load_message = document.createElement("h3");
-    load_message.innerText = "Loading...";
-    station_list?.appendChild(load_message);
-
+    // big try catch to catch literally everything - gross and terrible form, I know... but we ain't building a watch
+    // here - though I want it to timeout after 5 tries
+    let attempt_counter = 0;
     try{
+        attempt_counter = attempt_counter + 1;
+
+        console.log("loading data");
+        let station_list = document.getElementById("station_list");
+        let search_bar = document.getElementById("search_bar")
+        let bus_stations = '';
+        let load_message = document.createElement("h3");
+        load_message.innerText = "Loading...";
+        station_list?.appendChild(load_message);
+
         // fetch the data from the API
         fetch('https://h4nmsf5uxc2kytzpco74ds5ody0mffge.lambda-url.us-west-2.on.aws/')
         .then(function (response) {
@@ -53,7 +58,12 @@ function populate() {
         });
     } catch {
         // uh oh, something bad happened let's try again
-        populate();
+        if (attempt_counter >= 5){
+            console.log('Major error...')
+        } else{
+            populate();
+        }
+
     }
 
 
